b9dd4f6a839e702449f495b0865cfc6d4d195cdf
Lukas Barth <mail@tinloaf.de>
2018-12-24 00:21:02 +0100

    Remove code from pages branch

--- pricegrabber/exceptions.py
+++
@@ -1,8 +0,0 @@
-class NetworkException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-
-class ConfigException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
